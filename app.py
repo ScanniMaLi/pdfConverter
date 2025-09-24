2d1b17d1bdacb60f866fe2eb8419df545398c0f0
ScanniMaLi <lisaliebich@msn.com>
2025-09-24 11:14:53 +0200

    fixed a big with uploads folder

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from io import BytesIO
 import zipfile
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = './uploads'
+app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(os.path.abspath(file)), 'uploads')
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB max file size
 
 def convert_image_to_pdf(input_path, output_path):
